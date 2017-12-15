770e7c748a989398f4b0510f9921d77d0565044d
Pedro Campos <pedrofreitascampos@gmail.com>
2017-12-15 15:12:27 +0000

    adding FAQ button, removing banner - fixes #24

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+faq_html = "<div id='faq' style='overflow:scroll' onclick=unShowFaq()><img src='assets/img/sejaondefor.jpg' width='100%' height='40%'><br><br><div class='question'><b>O que é o <a href=3D'http://sejaondefor.com'>sejaondefor.com</a>?</b></div>" +
+    "<div><br></div>" +
+    "<div>O <a href=3D'http://sejaondefor.com'>sejaondefor.com</a> é o mostruário ou expositor do " +
+    "<a href='https://www.instagram.com/to.colante/'>to.colante</a> fornecendo a localização em mapa dos inúmeros" +
+    " to.colantes que têm vindo a ser espalhados desde 2015." +
+    "</div>" +
+    "<div><br>" +
+    "</div>" +
+    "<div class='question'><b>Já agora o que é o to.colante?</b></div>" +
+    "<div>" +
+    "<br></div>" +
+    "<div>O Benfica nasceu em Lisboa e desde sempre esteve aberto ao mundo. " +
+    "   Partindo desta premissa, o to.colante nasce do amor incondicional pelo " +
+    "Sport Lisboa e Benfica procurando e tendo o intuito de espalhar mística." +
+    "</div>" +
+    "<div><br>Partindo de uma lógica DIY (do it yourself), desde 2015 tem vindo a ser criados " +
+    "inúmeros stickers - os autodenominados to.colantes, usando uma linguagem visual mista, " +
+    "diversa mas acima de tudo positiva, de acordo com a história e valores do clube." +
+    "</div>" +
+    "<div><br>Com o intuito de chegar a mais pessoas (e a pedido de algumas famílias), " +
+    "isto redundou igualmente na criação de uma pagina na rede social " +
+    "<a href='https://www.instagram.com/to.colante/'>instagram</a>, onde para além de espalharmos a mística de " +
+    "modo digital, difundimos algumas das colagens já efectuadas." +
+    "</div>" +
+    "<div><br></div>" +
+    "<div class='question'><b>Ok ok...já percebi esse blá blá. Onde posso arranjar to.colantes?</b>" +
+    "</div>" +
+    "<div><br></div>" +
+    "<div>Bom...de certeza que não os encontrarás numa loja. Mas na Luz, em dia de jogo, com certeza irás " +
+    "encontrar alguém com to.colantes junto ao Manelito (roulote em" +
+    "   Alto dos Moinhos). Uma vez lá e dado que o Manel não dá informações gratuitas, depois de pagares " +
+    "uma cerveja ou o teu consumo, podes questioná-lo sobre onde poderá encontrar a &quot;malta " +
+    "dos stickers&quot;. Ele indicará aonde estaremos. Na eventualidade de seres abstémio, " +
+    "estares em dieta de retenção de líquidos, ou simplesmente viveres longe da Luz, " +
+    "podes entrar em contacto via mensagem (instagram ou <a href=mailto:info.tocolante@gmail.com'>info.tocolante@gmail.com</a>)." +
+    "</div>" +
+    "<div><br></div>" +
+    "<div class='question'><b>Como posso ter to.colantes cola" +
+    "   dos por mim publicados aqui na página?</b>" +
+    "</div>" +
+    "<div><br></div>" +
+    "<div>" +
+    "   Conhecer o tipo que gere isto ou alguém que conheça esse tipo e " +
+    "pagar-lhe umas cervejas (vide acima onde) é sempre uma boa táctica. " +
+    "   Caso isso não seja possível (ou não queiras) podes sempre enviar as fotos para " +
+    "<a href=3D'mailto:info.tocolante@gmail.com'>info.tocolante@gmail.com</a> . " +
+    "Desde que enquadradas na temática, todas as imagens " +
+    "   são bem vindas (com ou sem cerveja envolvida). Indicar a localização das ditas " +
+    "fotos era igualmente simpático. :)" +
+    "</div>" +
+    "<div><br></div>" +
+    "<div class='question'><b>Existe mesmo um Tó?</b></div>" +
+    "   <div><br></div>" +
+    "   <div>Sim e com o nome completo António José Conceição Oliveira " +
+    "- mais conhecido por Toni. E para quem escreve estas linhas, " +
+    "   é um dos Senhores Benfica. Mística no seu estado mais puro." +
+    "</div></div>"
+
 // Requires that leaflet and leaflet.instagram are loaded on global "L"
 function retrieveToken(tokenFile, callback){
     var rawFile = new XMLHttpRequest();
@@ -38,7 +96,6 @@ function sizeFactor(zoom) {
     else if (zoom >= 10) return 25;
 }
 
-
 var benfica_light = "https://api.mapbox.com/styles/v1/pedcampo/cjb56118t1bxu2rmsdu2pblnn/tiles/256/{z}/{x}/{y}";
 var benfica_banner = "https://api.mapbox.com/styles/v1/pedcampo/cjb6orkxz2sqf2spxmw3p3a8r/tiles/256/{z}/{x}/{y}";
 var benfica_odyssey = "https://api.mapbox.com/styles/v1/pedcampo/cjb537i4219g02smqopxgpsx4/tiles/256/{z}/{x}/{y}";
@@ -57,6 +114,24 @@ function load_map_tiles(token){
         accessToken: token
         }
     ).addTo(mymap);
+}
+
+function showFaq() {
+    var el = document.createElement('faq');
+    //el.setAttribute('id', 'ifrm');
+    //el.setAttribute('src', 'faq.html');
+    el.innerHTML = faq_html;
+    document.body.appendChild(el);
+    el = document.getElementById('faq_button');
+    el.style.visibility = 'hidden';
+}
+
+function unShowFaq() {
+    var el = document.getElementById('faq');
+    el.parentNode.removeChild(el);
+    el = document.getElementById('faq_button');
+    el.style.visibility = 'visible';
+
 }
 
 var mymap = L.map('mapid').setView([38.752678, -9.184681], 3);
