4c675696a059481e7ac9ca137d8d5f20a39d8bb3
Pedrow <pedrofreitascampos@gmail.com>
2026-03-23 09:54:50 +0000

    feat: add Águia experimental theme (red-tinted map, pulse clusters, SLB watermark)

--- docs/index.js
+++ docs/index.js
@@ -19,6 +19,10 @@ const TILES = {
   ultras: {
     url: 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  },
+  aguia: {
+    url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
   }
 };
 
